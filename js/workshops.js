88ebd0176e7284ad3d13567a769e5c0c71227751
gopalakrishnan-r <gopalakrishnars94@gmail.com>
2021-05-18 23:20:50 +0530

    workshops added

--- js/workshops.js
+++ js/workshops.js
@@ -6,12 +6,14 @@ $(document).ready(function() {
             var workshop = '';
             $.each(data, function(key, value) {
 
-                workshop += '<div class="col-lg-4 col-sm-4">'
+                workshop += '<div class="col-lg-4 col-md-6 col-sm-12">'
+                workshop += '<div class="text-center">'
                 workshop += '<img class="img-fluid shadow p-3 mb-5 bg-body rounded zoom work-shop" src="' + value.img + '">'
-                workshop += '<div class="align-self-baseline">'
-                workshop += '<h6 class="mb-2 mt-2 text-center">' + value.name + '</h6>'
-                workshop += '<p class="text-muted mb-0 text-center">' + value.date + '</p>'
-                workshop += '<br><div class="text-center mb-3"><a class="btn btn-primary" target="_blank" href="' + value.link + '">Visit</a></div>'
+                workshop += '</div>'
+                workshop += '<div class="text-center">'
+                workshop += '<h6 class="mt-0">' + value.name + '</h6>'
+                workshop += '<p class="text-muted mb-0 ">' + value.date + '</p>'
+                workshop += '<br><div class="mb-3"><a class="btn btn-primary" target="_blank" href="' + value.link + '">Visit</a></div>'
                 workshop += '</div>'
                 workshop += '</div>'
 
