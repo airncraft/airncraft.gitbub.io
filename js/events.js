d04b9c1c6e7b1969db458ca51605805182c58ee1
gopalakrishnan-r <gopalakrishnars94@gmail.com>
2021-05-18 22:41:12 +0530

    prozone event added

--- js/events.js
+++ js/events.js
@@ -6,9 +6,9 @@ $(document).ready(function() {
             var event = '';
             $.each(data, function(key, value) {
 
-                event += '<div class="col-lg-3 col-sm-3">'
+                event += '<div class="col-lg-4 col-md-6 col-sm-12">'
                 event += '<img class="img-fluid shadow p-3 mb-5 bg-body rounded zoom" src="' + value.img + '">'
-                event += '<div class="align-self-baseline">'
+                event += '<div class="text-center">'
                 event += '<h6 class="mb-2 mt-2">' + value.name + '</h6>'
                 event += '<p class="text-muted mb-0">' + value.date + '</p>'
                 event += '<br><div class="text-center"><a class="btn btn-primary" target="_blank" href="' + value.link + '">Visit</a></div>'
