c50521b69cb5abc19f719c56e1c092d850a647fe
gopalakrishnan-r <gopalakrishnars94@gmail.com>
2021-05-18 18:35:23 +0530

    gallery alignment issue corrected

--- js/gallery.js
+++ js/gallery.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     $.getJSON("/gallery/virtual-summer-camp-april-2020.json",
         function(data) {
             var gallery_title = '';
-            gallery_title += '<div class="col-lg-6 col-sm-4 text-center">'
+            gallery_title += '<div class="col-lg-12 col-md-12 col-sm-12 text-center">'
             gallery_title += '<h5 class="text-left font-weight-bold text-black">' + data[1].name + ' - ' + data[1].desc + '</h5>'
             gallery_title += '</div>'
             $('#imgcontainer_title').append(gallery_title);
@@ -13,7 +13,7 @@ $(document).ready(function() {
             var gallery = '';
             $.each(data, function(key, value) {
 
-                gallery += '<div class="col-lg-4 col-sm-4 text-center zoom">'
+                gallery += '<div class="col-lg-4 col-md-6 col-sm-6 text-center">'
                 gallery += '<div class="mt-5 mr-2">'
                 gallery += '<a class="portfolio-box" href="' + value.image1 + '">'
                 gallery += '<img class="img-fluid zoom edge work-shop" src="' + value.image + '">'
@@ -31,7 +31,7 @@ $(document).ready(function() {
     $.getJSON("/gallery/workshop-at-home-jan-2020.json",
         function(data) {
             var gallery_title = '';
-            gallery_title += '<div class="col-lg-6 col-sm-4 text-center">'
+            gallery_title += '<div class="col-lg-12 col-md-12 col-sm-12 text-center">'
             gallery_title += '<h5 class="text-left font-weight-bold text-black">' + data[1].name + ' - ' + data[1].desc + '</h5>'
             gallery_title += '</div>'
             $('#imgcontainer1_title').append(gallery_title);
@@ -39,7 +39,7 @@ $(document).ready(function() {
             var gallery = '';
             $.each(data, function(key, value) {
 
-                gallery += '<div class="col-lg-4 col-sm-4 text-center">'
+                gallery += '<div class="col-lg-4 col-md-6 col-sm-6 text-center">'
                 gallery += '<div class="mt-5 zoom">'
                 gallery += '<a class="portfolio-box" href="' + value.image1 + '">'
                 gallery += '<img class="img-fluid edge work-shop" src="' + value.image + '">'
@@ -57,7 +57,7 @@ $(document).ready(function() {
     $.getJSON("/gallery/combo-workshop-jan-2020.json",
         function(data) {
             var gallery_title = '';
-            gallery_title += '<div class="col-lg-6 col-sm-4 text-center">'
+            gallery_title += '<div class="col-lg-12 col-md-12 col-sm-12 text-center">'
             gallery_title += '<h5 class="text-left font-weight-bold text-black">' + data[1].name + ' - ' + data[1].desc + '</h5>'
             gallery_title += '</div>'
             $('#imgcontainer2_title').append(gallery_title);
@@ -65,7 +65,7 @@ $(document).ready(function() {
             var gallery = '';
             $.each(data, function(key, value) {
 
-                gallery += '<div class="col-lg-4 col-sm-4 text-center">'
+                gallery += '<div class="col-lg-4 col-md-6 col-sm-6 text-center">'
                 gallery += '<div class="mt-5 zoom">'
                 gallery += '<a class="portfolio-box" href="' + value.image1 + '">'
                 gallery += '<img class="img-fluid edge work-shop" src="' + value.image + '">'
@@ -83,7 +83,7 @@ $(document).ready(function() {
     $.getJSON("/gallery/crafty-christmas-workshop-dec-2019.json",
         function(data) {
             var gallery_title = '';
-            gallery_title += '<div class="col-lg-6 col-sm-4 text-center">'
+            gallery_title += '<div class="col-lg-12 col-md-12 col-sm-12 text-center">'
             gallery_title += '<h5 class="text-left font-weight-bold text-black">' + data[1].name + ' - ' + data[1].desc + '</h5>'
             gallery_title += '</div>'
             $('#imgcontainer3_title').append(gallery_title);
@@ -91,7 +91,7 @@ $(document).ready(function() {
             var gallery = '';
             $.each(data, function(key, value) {
 
-                gallery += '<div class="col-lg-4 col-sm-4 text-center">'
+                gallery += '<div class="col-lg-4 col-md-6 col-sm-6 text-center">'
                 gallery += '<div class="mt-5 zoom">'
                 gallery += '<a class="portfolio-box" href="' + value.image1 + '">'
                 gallery += '<img class="img-fluid edge work-shop" src="' + value.image + '">'
@@ -109,7 +109,7 @@ $(document).ready(function() {
     $.getJSON("/gallery/paint-and-plane-december-2019.json",
         function(data) {
             var gallery_title = '';
-            gallery_title += '<div class="col-lg-6 col-sm-4 text-center">'
+            gallery_title += '<div class="col-lg-12 col-md-12 col-sm-12 text-center">'
             gallery_title += '<h5 class="text-left font-weight-bold text-black">' + data[1].name + ' - ' + data[1].desc + '</h5>'
             gallery_title += '</div>'
             $('#imgcontainer4_title').append(gallery_title);
@@ -117,7 +117,7 @@ $(document).ready(function() {
             var gallery = '';
             $.each(data, function(key, value) {
 
-                gallery += '<div class="col-lg-4 col-sm-4 text-center">'
+                gallery += '<div class="col-lg-4 col-md-6 col-sm-6 text-center">'
                 gallery += '<div class="mt-5 zoom">'
                 gallery += '<a class="portfolio-box" href="' + value.image1 + '">'
                 gallery += '<img class="img-fluid edge work-shop" src="' + value.image + '">'
@@ -135,7 +135,7 @@ $(document).ready(function() {
     $.getJSON("/gallery/paint-and-plane_nov-2019.json",
         function(data) {
             var gallery_title = '';
-            gallery_title += '<div class="col-lg-6 col-sm-4 text-center">'
+            gallery_title += '<div class="col-lg-12 col-md-12 col-sm-12 text-center">'
             gallery_title += '<h5 class="text-left font-weight-bold text-black">' + data[1].name + ' - ' + data[1].desc + '</h5>'
             gallery_title += '</div>'
             $('#imgcontainer5_title').append(gallery_title);
@@ -143,7 +143,7 @@ $(document).ready(function() {
             var gallery = '';
             $.each(data, function(key, value) {
 
-                gallery += '<div class="col-lg-4 col-sm-4 text-center">'
+                gallery += '<div class="col-lg-4 col-md-6 col-sm-6 text-center">'
                 gallery += '<div class="mt-5 zoom">'
                 gallery += '<a class="portfolio-box" href="' + value.image1 + '">'
                 gallery += '<img class="img-fluid edge work-shop" src="' + value.image + '">'
@@ -161,7 +161,7 @@ $(document).ready(function() {
     $.getJSON("/gallery/airncraft_event_and_contest_2019.json",
         function(data) {
             var gallery_title = '';
-            gallery_title += '<div class="col-lg-6 col-sm-4 text-center">'
+            gallery_title += '<div class="col-lg-12 col-md-12 col-sm-12 text-center">'
             gallery_title += '<h5 class="text-left font-weight-bold text-black">' + data[1].name + ' - ' + data[1].desc + '</h5>'
             gallery_title += '</div>'
             $('#imgcontainer6_title').append(gallery_title);
@@ -169,7 +169,7 @@ $(document).ready(function() {
             var gallery = '';
             $.each(data, function(key, value) {
 
-                gallery += '<div class="col-lg-4 col-sm-4 text-center">'
+                gallery += '<div class="col-lg-4 col-md-6 col-sm-6 text-center">'
                 gallery += '<div class="mt-5 zoom work-shop">'
                 gallery += '<a class="portfolio-box" href="' + value.image1 + '">'
                 gallery += '<img class="img-fluid edge work-shop" src="' + value.image + '">'
@@ -187,7 +187,7 @@ $(document).ready(function() {
     $.getJSON("/gallery/glider-and-popup-card-workshop-sep-2019.json",
         function(data) {
             var gallery_title = '';
-            gallery_title += '<div class="col-lg-6 col-sm-4 text-center">'
+            gallery_title += '<div class="col-lg-12 col-md-12 col-sm-12 text-center">'
             gallery_title += '<h5 class="text-left font-weight-bold text-black">' + data[1].name + ' - ' + data[1].desc + '</h5>'
             gallery_title += '</div>'
             $('#imgcontainer7_title').append(gallery_title);
@@ -195,7 +195,7 @@ $(document).ready(function() {
             var gallery = '';
             $.each(data, function(key, value) {
 
-                gallery += '<div class="col-lg-4 col-sm-4 text-center">'
+                gallery += '<div class="col-lg-4 col-md-6 col-sm-6 text-center">'
                 gallery += '<div class="mt-5 zoom work-shop">'
                 gallery += '<a class="portfolio-box" href="' + value.image1 + '">'
                 gallery += '<img class="img-fluid edge work-shop" src="' + value.image + '">'
@@ -213,7 +213,7 @@ $(document).ready(function() {
     $.getJSON("/gallery/csr-activity-september-2019.json",
         function(data) {
             var gallery_title = '';
-            gallery_title += '<div class="col-lg-6 col-sm-4 text-center">'
+            gallery_title += '<div class="col-lg-12 col-md-12 col-sm-12 text-center">'
             gallery_title += '<h5 class="text-left font-weight-bold text-black">' + data[1].name + ' - ' + data[1].desc + '</h5>'
             gallery_title += '</div>'
             $('#imgcontainer8_title').append(gallery_title);
@@ -221,7 +221,7 @@ $(document).ready(function() {
             var gallery = '';
             $.each(data, function(key, value) {
 
-                gallery += '<div class="col-lg-4 col-sm-4 text-center">'
+                gallery += '<div class="col-lg-4 col-md-6 col-sm-6 text-center">'
                 gallery += '<div class="mt-5 zoom">'
                 gallery += '<a class="portfolio-box" href="' + value.image1 + '">'
                 gallery += '<img class="img-fluid edge work-shop" src="' + value.image + '">'
@@ -239,7 +239,7 @@ $(document).ready(function() {
     $.getJSON("/gallery/rakshabandan-hamper-and-rc-plane-workshop-aug-2019.json",
         function(data) {
             var gallery_title = '';
-            gallery_title += '<div class="col-lg-6 col-sm-4 text-center">'
+            gallery_title += '<div class="col-lg-12 col-md-12 col-sm-12 text-center">'
             gallery_title += '<h5 class="text-left font-weight-bold text-black">' + data[1].name + ' - ' + data[1].desc + '</h5>'
             gallery_title += '</div>'
             $('#imgcontainer9_title').append(gallery_title);
@@ -247,7 +247,7 @@ $(document).ready(function() {
             var gallery = '';
             $.each(data, function(key, value) {
 
-                gallery += '<div class="col-lg-4 col-sm-4 text-center">'
+                gallery += '<div class="col-lg-4 col-md-6 col-sm-6 text-center">'
                 gallery += '<div class="mt-5 zoom">'
                 gallery += '<a class="portfolio-box" href="' + value.image1 + '">'
                 gallery += '<img class="img-fluid edge work-shop" src="' + value.image + '">'
@@ -266,7 +266,7 @@ $(document).ready(function() {
     $.getJSON("/gallery/arts-crafts-workshop-July-2019.json",
         function(data) {
             var gallery_title = '';
-            gallery_title += '<div class="col-lg-6 col-sm-4 text-center">'
+            gallery_title += '<div class="col-lg-12 col-md-12 col-sm-12 text-center">'
             gallery_title += '<h5 class="text-left font-weight-bold text-black">' + data[1].name + ' - ' + data[1].desc + '</h5>'
             gallery_title += '</div>'
             $('#imgcontainer10_title').append(gallery_title);
@@ -274,7 +274,7 @@ $(document).ready(function() {
             var gallery = '';
             $.each(data, function(key, value) {
 
-                gallery += '<div class="col-lg-4 col-sm-4 text-center">'
+                gallery += '<div class="col-lg-4 col-md-6 col-sm-6 text-center">'
                 gallery += '<div class="mt-5 zoom">'
                 gallery += '<a class="portfolio-box" href="' + value.image1 + '">'
                 gallery += '<img class="img-fluid edge work-shop" src="' + value.image + '">'
@@ -293,7 +293,7 @@ $(document).ready(function() {
      $.getJSON("/gallery/made-with-scraps-june.json",
         function(data) {
             var gallery_title = '';
-            gallery_title += '<div class="col-lg-6 col-sm-4 text-center">'
+            gallery_title += '<div class="col-lg-12 col-md-12 col-sm-12 text-center">'
             gallery_title += '<h5 class="text-left font-weight-bold text-black">' + data[1].name + ' - ' + data[1].desc + '</h5>'
             gallery_title += '</div>'
             $('#imgcontainer11_title').append(gallery_title);
@@ -301,7 +301,7 @@ $(document).ready(function() {
             var gallery = '';
             $.each(data, function(key, value) {
 
-                gallery += '<div class="col-lg-4 col-sm-4 text-center">'
+                gallery += '<div class="col-lg-4 col-md-6 col-sm-6 text-center">'
                 gallery += '<div class="mt-5 zoom">'
                 gallery += '<a class="portfolio-box" href="' + value.image1 + '">'
                 gallery += '<img class="img-fluid edge work-shop" src="' + value.image + '">'
@@ -320,7 +320,7 @@ $(document).ready(function() {
      $.getJSON("/gallery/balsa-glider-and-canvas-nameplate-workshop-june-2019.json",
         function(data) {
             var gallery_title = '';
-            gallery_title += '<div class="col-lg-6 col-sm-4 text-center">'
+            gallery_title += '<div class="col-lg-12 col-md-12 col-sm-12 text-center">'
             gallery_title += '<h5 class="text-left font-weight-bold text-black">' + data[1].name + ' - ' + data[1].desc + '</h5>'
             gallery_title += '</div>'
             $('#imgcontainer12_title').append(gallery_title);
@@ -328,7 +328,7 @@ $(document).ready(function() {
             var gallery = '';
             $.each(data, function(key, value) {
 
-                gallery += '<div class="col-lg-4 col-sm-4 text-center">'
+                gallery += '<div class="col-lg-4 col-md-6 col-sm-6 text-center">'
                 gallery += '<div class="mt-5 zoom">'
                 gallery += '<a class="portfolio-box" href="' + value.image1 + '">'
                 gallery += '<img class="img-fluid edge work-shop" src="' + value.image + '">'
@@ -347,7 +347,7 @@ $(document).ready(function() {
      $.getJSON("/gallery/summer-camp-may-2019.json",
         function(data) {
             var gallery_title = '';
-            gallery_title += '<div class="col-lg-6 col-sm-4 text-center">'
+            gallery_title += '<div class="col-lg-12 col-md-12 col-sm-12 text-center">'
             gallery_title += '<h5 class="text-left font-weight-bold text-black">' + data[1].name + ' - ' + data[1].desc + '</h5>'
             gallery_title += '</div>'
             $('#imgcontainer13_title').append(gallery_title);
@@ -355,7 +355,7 @@ $(document).ready(function() {
             var gallery = '';
             $.each(data, function(key, value) {
 
-                gallery += '<div class="col-lg-4 col-sm-4 text-center">'
+                gallery += '<div class="col-lg-4 col-md-6 col-sm-6 text-center">'
                 gallery += '<div class="mt-5 zoom">'
                 gallery += '<a class="portfolio-box" href="' + value.image1 + '">'
                 gallery += '<img class="img-fluid edge work-shop" src="' + value.image + '">'
@@ -375,7 +375,7 @@ $(document).ready(function() {
      $.getJSON("/gallery/kovai-vizha-aeromodelling-workshop-january-2019.json",
         function(data) {
             var gallery_title = '';
-            gallery_title += '<div class="col-lg-6 col-sm-4 text-center">'
+            gallery_title += '<div class="col-lg-12 col-md-12 col-sm-12 text-center">'
             gallery_title += '<h5 class="text-left font-weight-bold text-black">' + data[1].name + ' - ' + data[1].desc + '</h5>'
             gallery_title += '</div>'
             $('#imgcontainer14_title').append(gallery_title);
@@ -383,7 +383,7 @@ $(document).ready(function() {
             var gallery = '';
             $.each(data, function(key, value) {
 
-                gallery += '<div class="col-lg-4 col-sm-4 text-center">'
+                gallery += '<div class="col-lg-4 col-md-6 col-sm-6 text-center">'
                 gallery += '<div class="mt-5 zoom">'
                 gallery += '<a class="portfolio-box" href="' + value.image1 + '">'
                 gallery += '<img class="img-fluid edge work-shop" src="' + value.image + '">'
@@ -401,7 +401,7 @@ $(document).ready(function() {
      $.getJSON("/gallery/kovai-vizha–canvas-and-balsa-plane-workshop-january-2019.json",
         function(data) {
             var gallery_title = '';
-            gallery_title += '<div class="col-lg-6 col-sm-4 text-center">'
+            gallery_title += '<div class="col-lg-12 col-md-12 col-sm-12 text-center">'
             gallery_title += '<h5 class="text-left font-weight-bold text-black">' + data[1].name + ' - ' + data[1].desc + '</h5>'
             gallery_title += '</div>'
             $('#imgcontainer15_title').append(gallery_title);
@@ -409,7 +409,7 @@ $(document).ready(function() {
             var gallery = '';
             $.each(data, function(key, value) {
 
-                gallery += '<div class="col-lg-4 col-sm-4 text-center">'
+                gallery += '<div class="col-lg-4 col-md-6 col-sm-6 text-center">'
                 gallery += '<div class="mt-5 zoom">'
                 gallery += '<a class="portfolio-box" href="' + value.image1 + '">'
                 gallery += '<img class="img-fluid edge work-shop" src="' + value.image + '">'
